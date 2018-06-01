16d94f8449a8f2424a717a30f6c789d3eba36220
Andrew Scott <adscott1982@googlemail.com>
2018-06-01 22:02:21 +0100

    Added remaining group fixtures.

--- WorldCupPredictor/Data/Group.cs
+++ WorldCupPredictor/Data/Group.cs
@@ -58,48 +58,96 @@ namespace WorldCupPredictor.Data
             {
                 new Match(1, new DateTime(2018, 6, 14, 15, 0, 0, DateTimeKind.Utc), Teams.Russia, Teams.SaudiArabia),
                 new Match(2, new DateTime(2018, 6, 15, 12, 0, 0, DateTimeKind.Utc), Teams.Egypt, Teams.Uruguay),
+
+                new Match(3, new DateTime(2018, 6, 19, 18, 0, 0, DateTimeKind.Utc), Teams.Russia, Teams.Egypt),
+                new Match(4, new DateTime(2018, 6, 20, 15, 0, 0, DateTimeKind.Utc), Teams.Uruguay, Teams.SaudiArabia),
+
+                new Match(5, new DateTime(2018, 6, 25, 14, 0, 0, DateTimeKind.Utc), Teams.SaudiArabia, Teams.Egypt),
+                new Match(6, new DateTime(2018, 6, 25, 14, 0, 0, DateTimeKind.Utc), Teams.Uruguay, Teams.Russia)
             });
 
             this.b = new Group("B", new List<Match>
             {
-                new Match(3, new DateTime(2018, 6, 15, 15, 0, 0, DateTimeKind.Utc), Teams.Morocco, Teams.Iran),
-                new Match(4, new DateTime(2018, 6, 15, 18, 0, 0, DateTimeKind.Utc), Teams.Portugal, Teams.Spain)
+                new Match(7, new DateTime(2018, 6, 15, 15, 0, 0, DateTimeKind.Utc), Teams.Morocco, Teams.Iran),
+                new Match(8, new DateTime(2018, 6, 15, 18, 0, 0, DateTimeKind.Utc), Teams.Portugal, Teams.Spain),
+
+                new Match(9, new DateTime(2018, 6, 20, 12, 0, 0, DateTimeKind.Utc), Teams.Portugal, Teams.Morocco),
+                new Match(10, new DateTime(2018, 6, 20, 18, 0, 0, DateTimeKind.Utc), Teams.Iran, Teams.Spain),
+
+                new Match(11, new DateTime(2018, 6, 25, 18, 0, 0, DateTimeKind.Utc), Teams.Iran, Teams.Portugal),
+                new Match(12, new DateTime(2018, 6, 25, 18, 0, 0, DateTimeKind.Utc), Teams.Spain, Teams.Morocco)
             });
 
             this.c = new Group("C", new List<Match>
             {
-                new Match(5, new DateTime(2018, 6, 16, 10, 0, 0, DateTimeKind.Utc), Teams.France, Teams.Australia),
-                new Match(6, new DateTime(2018, 6, 16, 16, 0, 0, DateTimeKind.Utc), Teams.Peru, Teams.Denmark)
+                new Match(13, new DateTime(2018, 6, 16, 10, 0, 0, DateTimeKind.Utc), Teams.France, Teams.Australia),
+                new Match(14, new DateTime(2018, 6, 16, 16, 0, 0, DateTimeKind.Utc), Teams.Peru, Teams.Denmark),
+
+                new Match(15, new DateTime(2018, 6, 21, 12, 0, 0, DateTimeKind.Utc), Teams.Denmark, Teams.Australia),
+                new Match(16, new DateTime(2018, 6, 21, 15, 0, 0, DateTimeKind.Utc), Teams.France, Teams.Peru),
+
+                new Match(17, new DateTime(2018, 6, 26, 14, 0, 0, DateTimeKind.Utc), Teams.Australia, Teams.Peru),
+                new Match(18, new DateTime(2018, 6, 26, 14, 0, 0, DateTimeKind.Utc), Teams.Denmark, Teams.France)
             });
 
             this.d = new Group("D", new List<Match>
             {
-                new Match(7, new DateTime(2018, 6, 16, 13, 0, 0, DateTimeKind.Utc), Teams.Argentina, Teams.Iceland),
-                new Match(8, new DateTime(2018, 6, 16, 19, 0, 0, DateTimeKind.Utc), Teams.Croatia, Teams.Nigeria)
+                new Match(19, new DateTime(2018, 6, 16, 13, 0, 0, DateTimeKind.Utc), Teams.Argentina, Teams.Iceland),
+                new Match(20, new DateTime(2018, 6, 16, 19, 0, 0, DateTimeKind.Utc), Teams.Croatia, Teams.Nigeria),
+
+                new Match(21, new DateTime(2018, 6, 21, 18, 0, 0, DateTimeKind.Utc), Teams.Argentina, Teams.Croatia),
+                new Match(22, new DateTime(2018, 6, 22, 15, 0, 0, DateTimeKind.Utc), Teams.Nigeria, Teams.Iceland),
+
+                new Match(23, new DateTime(2018, 6, 26, 18, 0, 0, DateTimeKind.Utc), Teams.Iceland, Teams.Croatia),
+                new Match(24, new DateTime(2018, 6, 26, 18, 0, 0, DateTimeKind.Utc), Teams.Nigeria, Teams.Argentina)
             });
 
             this.e = new Group("E", new List<Match>
             {
-                new Match(9, new DateTime(2018, 6, 17, 12, 0, 0, DateTimeKind.Utc), Teams.CostaRica, Teams.Serbia),
-                new Match(10, new DateTime(2018, 6, 17, 18, 0, 0, DateTimeKind.Utc), Teams.Brazil, Teams.Switzerland)
+                new Match(25, new DateTime(2018, 6, 17, 12, 0, 0, DateTimeKind.Utc), Teams.CostaRica, Teams.Serbia),
+                new Match(26, new DateTime(2018, 6, 17, 18, 0, 0, DateTimeKind.Utc), Teams.Brazil, Teams.Switzerland),
+
+                new Match(27, new DateTime(2018, 6, 22, 12, 0, 0, DateTimeKind.Utc), Teams.Brazil, Teams.CostaRica),
+                new Match(28, new DateTime(2018, 6, 22, 18, 0, 0, DateTimeKind.Utc), Teams.Serbia, Teams.Switzerland),
+
+                new Match(29, new DateTime(2018, 6, 27, 18, 0, 0, DateTimeKind.Utc), Teams.Serbia, Teams.Brazil),
+                new Match(30, new DateTime(2018, 6, 27, 18, 0, 0, DateTimeKind.Utc), Teams.Switzerland, Teams.CostaRica)
             });
 
             this.f = new Group("F", new List<Match>
             {
-                new Match(11, new DateTime(2018, 6, 17, 15, 0, 0, DateTimeKind.Utc), Teams.Germany, Teams.Mexico),
-                new Match(12, new DateTime(2018, 6, 18, 12, 0, 0, DateTimeKind.Utc), Teams.Sweden, Teams.SouthKorea)
+                new Match(31, new DateTime(2018, 6, 17, 15, 0, 0, DateTimeKind.Utc), Teams.Germany, Teams.Mexico),
+                new Match(32, new DateTime(2018, 6, 18, 12, 0, 0, DateTimeKind.Utc), Teams.Sweden, Teams.SouthKorea),
+
+                new Match(33, new DateTime(2018, 6, 23, 15, 0, 0, DateTimeKind.Utc), Teams.SouthKorea, Teams.Mexico),
+                new Match(34, new DateTime(2018, 6, 23, 18, 0, 0, DateTimeKind.Utc), Teams.Germany, Teams.Sweden),
+
+                new Match(35, new DateTime(2018, 6, 27, 14, 0, 0, DateTimeKind.Utc), Teams.SouthKorea, Teams.Germany),
+                new Match(36, new DateTime(2018, 6, 27, 14, 0, 0, DateTimeKind.Utc), Teams.Mexico, Teams.Sweden)
             });
 
             this.g = new Group("G", new List<Match>
             {
-                new Match(13, new DateTime(2018, 6, 18, 15, 0, 0, DateTimeKind.Utc), Teams.Belgium, Teams.Panama),
-                new Match(14, new DateTime(2018, 6, 18, 18, 0, 0, DateTimeKind.Utc), Teams.Tunisia, Teams.England)
+                new Match(37, new DateTime(2018, 6, 18, 15, 0, 0, DateTimeKind.Utc), Teams.Belgium, Teams.Panama),
+                new Match(38, new DateTime(2018, 6, 18, 18, 0, 0, DateTimeKind.Utc), Teams.Tunisia, Teams.England),
+
+                new Match(39, new DateTime(2018, 6, 23, 12, 0, 0, DateTimeKind.Utc), Teams.Belgium, Teams.Tunisia),
+                new Match(40, new DateTime(2018, 6, 24, 12, 0, 0, DateTimeKind.Utc), Teams.England, Teams.Panama),
+
+                new Match(41, new DateTime(2018, 6, 28, 18, 0, 0, DateTimeKind.Utc), Teams.England, Teams.Belgium),
+                new Match(42, new DateTime(2018, 6, 28, 18, 0, 0, DateTimeKind.Utc), Teams.Panama, Teams.Tunisia)
             });
 
             this.h = new Group("H", new List<Match>
             {
-                new Match(15, new DateTime(2018, 6, 19, 12, 0, 0, DateTimeKind.Utc), Teams.Colombia, Teams.Japan),
-                new Match(16, new DateTime(2018, 6, 19, 15, 0, 0, DateTimeKind.Utc), Teams.Poland, Teams.Senegal)
+                new Match(43, new DateTime(2018, 6, 19, 12, 0, 0, DateTimeKind.Utc), Teams.Colombia, Teams.Japan),
+                new Match(44, new DateTime(2018, 6, 19, 15, 0, 0, DateTimeKind.Utc), Teams.Poland, Teams.Senegal),
+
+                new Match(45, new DateTime(2018, 6, 24, 15, 0, 0, DateTimeKind.Utc), Teams.Japan, Teams.Senegal),
+                new Match(46, new DateTime(2018, 6, 24, 18, 0, 0, DateTimeKind.Utc), Teams.Poland, Teams.Colombia),
+
+                new Match(47, new DateTime(2018, 6, 28, 14, 0, 0, DateTimeKind.Utc), Teams.Japan, Teams.Poland),
+                new Match(48, new DateTime(2018, 6, 28, 14, 0, 0, DateTimeKind.Utc), Teams.Senegal, Teams.Colombia)
             });
         }
 
